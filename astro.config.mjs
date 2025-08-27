0dab22664e2688d69429b46fc5fcff2fb237865b
Rozalia "Rüya" San <150453194+gridmouth@users.noreply.github.com>
2025-08-27 18:17:48 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://gridmouth.github.io",
-	base: "/",
+	base: "/eyewallwx",
 	trailingSlash: "always",
 
 	integrations: [
