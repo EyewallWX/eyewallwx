3e401b18d6b1ee83ed599b7cd08302514a58f035
Rozalia "Rüya" San <rozalia@nisemono.org>
2025-08-28 14:46:12 +0200

    Added tier 3 glossary

--- src/config.ts
+++ src/config.ts
@@ -10,27 +10,26 @@ import { LinkPreset } from "./types/config";
 export const siteConfig: SiteConfig = {
 	title: "EyewallWX",
 	subtitle: "Home",
-	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	lang: "en",
 	themeColor: {
-		hue: 300, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: true, // Hide the theme color picker for visitors
+		hue: 300,
+		fixed: true,
 	},
 	banner: {
 		enable: true,
-		src: "/assets/eyewallwx-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+		src: "/assets/eyewallwx-banner.webp",
+		position: "center",
 		credit: {
-			enable: true, // Display the credit text of the banner image
-			text: "JMA", // Credit text to be displayed
+			enable: true,
+			text: "JMA",
 			url: "https://www.jma.go.jp/jma/index.html",
 		},
 	},
 	toc: {
-		enable: true, // Display the table of contents on the right side of the post
-		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
+		enable: true,
+		depth: 2,
 	},
 	favicon: [
-		// Leave this array empty to use the default favicon
 		{
 			src: "/favicon/favicon-light-32.png",
 			theme: "light",
@@ -62,17 +61,22 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "Discord",
-			url: " https://discord.gg/HTyfKmwX9v", // Internal links should not include the base path, as it is automatically added
-			external: true, // Show an external link icon and will open in a new tab
+			url: "https://discord.gg/HTyfKmwX9v",
+			external: true,
 		},
 	],
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "/assets/eyewallwx-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "/assets/eyewallwx-avatar.webp",
 	name: "EyewallWX",
 	bio: "An open-to-all community of weather enthusiasts. We track tropical cyclones and tornado outbreaks, store archived data, and post live updates. Feel free to join our Discord server and follow us on social media!",
 		links: [
+		{
+			name: "Discord",
+			icon: "fa6-brands:discord",
+			url: "https://discord.gg/HTyfKmwX9v",
+		},
 		{
 			name: "Twitter",
 			icon: "fa6-brands:twitter",
@@ -93,7 +97,5 @@ export const licenseConfig: LicenseConfig = {
 };
 
 export const expressiveCodeConfig: ExpressiveCodeConfig = {
-	// Note: Some styles (such as background color) are being overridden, see the astro.config.mjs file.
-	// Please select a dark theme, as this blog theme currently only supports dark background color
 	theme: "github-dark",
 };
